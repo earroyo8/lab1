3b565ad669aa2ee155cbfff2fdc0a08b30ff415c
Student <student@lab-240-022.cs.csubak.edu>
2023-01-24 09:26:27 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,15 @@
+//Estevan Arroyo
+//lab-1 jan 24
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hellow World\n";
+
+    return 0;
+}
